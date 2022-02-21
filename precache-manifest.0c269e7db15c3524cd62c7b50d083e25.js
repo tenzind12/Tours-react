8d3ef01bd4071c27c05ec6bf0c78735346fc4ce2
Tenzin <dhontenzin@gmail.com>
2022-02-21 20:00:06 +0100

    Updates

---
+++ precache-manifest.0c269e7db15c3524cd62c7b50d083e25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a3cf946a9b8cee52e716108f0c16af",
+    "url": "/Tours-react/index.html"
+  },
+  {
+    "revision": "631dadcd1072ca4016ca",
+    "url": "/Tours-react/static/css/main.d2be02af.chunk.css"
+  },
+  {
+    "revision": "047f6c63a34c086323ae",
+    "url": "/Tours-react/static/js/2.2d7c86c6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Tours-react/static/js/2.2d7c86c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631dadcd1072ca4016ca",
+    "url": "/Tours-react/static/js/main.3ecbf31b.chunk.js"
+  },
+  {
+    "revision": "1305adbc6055187dc3bf",
+    "url": "/Tours-react/static/js/runtime-main.0b315f82.js"
+  }
+]);
